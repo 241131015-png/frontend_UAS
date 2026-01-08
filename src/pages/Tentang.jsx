264ee34d42591a88eadef2ec3d245b9f1e65419c
241131015-png <241131015@mhs.ubhinus.ac.id>
2026-01-09 00:50:52 +0700

    update style

--- src/pages/Tentang.jsx
+++ src/pages/Tentang.jsx
@@ -2,8 +2,8 @@ export default function Tentang() {
     return (
         <div className="page-container">
             <h2>Tentang Aplikasi Todo</h2>
-            <p>Aplikasi ini dibangun menggunakan React dan Context API untuk manajemen tema.</p>
-            <p>Data disimpan secara lokal di browser (LocalStorage).</p>
+            <p>Aplikasi To-Do List adalah aplikasi pencatat tugas harian yang membantu pengguna mengatur aktivitas secara lebih terstruktur dan produktif. Pengguna dapat menambahkan, mengedit, menandai tugas yang sudah selesai, serta menghapus tugas yang tidak diperlukan.</p>
+            <p>Dengan antarmuka yang sederhana dan mudah digunakan, aplikasi ini dirancang untuk mendukung kebiasaan disiplin, mengurangi lupa tugas, dan membantu pengguna mencapai target harian dengan lebih terorganisir.</p>
         </div>
     );
 }
